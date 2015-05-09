121fd4449c96bc7327c970ff63242108238fb11e
Michael Skogberg <malliina123@gmail.com>
2015-05-09 16:02:54 +0300

    0.2.2

--- project/LoggerBuild.scala
+++ project/LoggerBuild.scala
@@ -2,24 +2,25 @@ import bintray.Plugin.bintraySettings
 import com.mle.sbtutils.{SbtProjects, SbtUtils}
 import sbt.Keys._
 import sbt._
-
+import SbtUtils.{gitUserName, developerName}
 /**
  * A scala build file logger.
  */
 object LoggerBuild extends Build {
 
-  lazy val project = SbtProjects.mavenPublishProject("logback-rx").settings(projectSettings: _*)
+  lazy val project = SbtProjects.testableProject("logback-rx").settings(projectSettings: _*)
 
   lazy val projectSettings = bintraySettings ++ Seq(
-    version := "0.2.1",
-    SbtUtils.gitUserName := "malliina",
-    SbtUtils.developerName := "Michael Skogberg",
+    version := "0.2.2",
+    gitUserName := "malliina",
+    organization := s"com.github.${gitUserName.value}",
+    developerName := "Michael Skogberg",
     scalaVersion := "2.11.6",
     fork in Test := true,
     libraryDependencies ++= Seq(
       "org.slf4j" % "slf4j-api" % "1.7.12",
-      "io.reactivex" %% "rxscala" % "0.24.0",
-      "com.typesafe.play" %% "play-json" % "2.3.8",
+      "io.reactivex" %% "rxscala" % "0.24.1",
+      "com.typesafe.play" %% "play-json" % "2.3.9",
       "ch.qos.logback" % "logback-classic" % "1.1.3",
       "ch.qos.logback" % "logback-core" % "1.1.3",
       "org.scala-lang.modules" %% "scala-xml" % "1.0.2" % "test"
