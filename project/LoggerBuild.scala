aa768fa93aecfc9d1cc0a04d6d63153061ac4994
Michael Skogberg <malliina123@gmail.com>
2017-01-20 21:44:48 +0100

    Up deps

--- project/LoggerBuild.scala
+++ project/LoggerBuild.scala
@@ -1,5 +1,3 @@
-import bintray.Plugin.bintraySettings
-import bintray.Keys.{bintray, bintrayOrganization}
 import com.malliina.sbtutils.SbtProjects
 import com.malliina.sbtutils.SbtUtils.{developerName, gitUserName}
 import sbt.Keys._
@@ -9,25 +7,22 @@ import sbt._
   */
 object LoggerBuild {
 
-  lazy val project = SbtProjects.testableProject("logback-rx")
+  lazy val project = SbtProjects.mavenPublishProject("logback-rx")
     .settings(projectSettings: _*)
 
-  lazy val projectSettings = bintraySettings ++ Seq(
-    version := "1.0.2",
+  lazy val projectSettings = Seq(
+    version := "1.1.0",
     gitUserName := "malliina",
     organization := s"com.${gitUserName.value}",
     developerName := "Michael Skogberg",
     scalaVersion := "2.11.8",
-    fork in Test := true,
     libraryDependencies ++= Seq(
-      "org.slf4j" % "slf4j-api" % "1.7.21",
-      "io.reactivex" %% "rxscala" % "0.26.2",
-      "com.typesafe.play" %% "play-json" % "2.5.9",
-      "ch.qos.logback" % "logback-classic" % "1.1.7",
-      "ch.qos.logback" % "logback-core" % "1.1.7",
+      "org.slf4j" % "slf4j-api" % "1.7.22",
+      "io.reactivex" %% "rxscala" % "0.26.5",
+      "com.typesafe.play" %% "play-json" % "2.5.10",
+      "ch.qos.logback" % "logback-classic" % "1.1.8",
+      "ch.qos.logback" % "logback-core" % "1.1.8",
       "org.scala-lang.modules" %% "scala-xml" % "1.0.6" % Test
-    ),
-    licenses +=("MIT", url("http://opensource.org/licenses/MIT")),
-    bintrayOrganization in bintray := None
+    )
   )
 }
