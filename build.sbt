0c0d36cb596e0fa41f8e3ed5d625e8221fa597cb
Michael Skogberg <michael.skogberg@futurice.com>
2019-06-28 01:12:04 +0300

    Remove rx; update deps; adopt 2.13

--- build.sbt
+++ build.sbt
@@ -4,13 +4,14 @@ lazy val p = Project("logback-streams", file("."))
 gitUserName := "malliina"
 organization := "com.malliina"
 developerName := "Michael Skogberg"
-scalaVersion := "2.12.8"
+scalaVersion := "2.13.0"
+crossScalaVersions := "2.12.8" :: scalaVersion.value :: Nil
 libraryDependencies ++= Seq(
-  "org.slf4j" % "slf4j-api" % "1.7.25",
-  "io.reactivex" %% "rxscala" % "0.26.5",
-  "com.typesafe.akka" %% "akka-stream" % "2.5.20",
-  "com.typesafe.play" %% "play-json" % "2.7.1",
+  "org.slf4j" % "slf4j-api" % "1.7.26",
+  "com.typesafe.akka" %% "akka-stream" % "2.5.23",
+  "com.typesafe.play" %% "play-json" % "2.7.4",
   "ch.qos.logback" % "logback-classic" % "1.2.3",
   "ch.qos.logback" % "logback-core" % "1.2.3",
-  "org.scala-lang.modules" %% "scala-xml" % "1.1.1" % Test
+  "org.scala-lang.modules" %% "scala-xml" % "1.2.0" % Test,
+  "org.scalatest" %% "scalatest" % "3.0.8" % Test
 )
--- src/main/scala/com/malliina/logbackrx/RxLogback.scala
+++ src/main/scala/com/malliina/logbackrx/RxLogback.scala
@@ -1,27 +1,12 @@
 package com.malliina.logbackrx
 
-import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.{AppenderBase, CoreConstants}
-import rx.lang.scala.subjects.SerializedSubject
-import rx.lang.scala.{Observable, Observer}
 
 object RxLogback {
   val defaultFormatter = new TimeFormatter(CoreConstants.ISO8601_PATTERN)
 
   def defaultFormat(time: Long): String = {
     defaultFormatter format time
-  }
-
-  /** A Logback appender that provides an [[Observable]] of log events.
-    */
-  trait RxAppenderBase[E] extends AppenderBase[E] {
-    protected def subject: SerializedSubject[E]
-
-    protected def observer: Observer[E] = subject
-
-    def events: Observable[E] = subject
-
-    override def append(event: E): Unit = observer onNext event
   }
 
   trait TimeFormatting[E] extends AppenderBase[E] {
@@ -36,9 +21,4 @@ object RxLogback {
 
     def format(timeStamp: Long) = formatter.format(timeStamp)
   }
-
-  trait EventMapping extends RxAppenderBase[ILoggingEvent] with TimeFormatting[ILoggingEvent] {
-    lazy val logEvents = events.map(e => LogEvent.fromLogbackEvent(e, format))
-  }
-
 }
